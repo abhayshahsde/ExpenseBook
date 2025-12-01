253c57ee52a975f0dbe5c742b048ffd3928b9e84
Abhay Shah <abhayshahdev@gmail.com>
2025-12-01 17:58:52 +0530

    Created IExpenseRepository interface and ExpenseRepository class

---
+++ backend/ExpenseBook.Core/Interfaces/IExpenseRepository.cs
@@ -0,0 +1,17 @@
+﻿using ExpenseBook.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpenseBook.Core.Interfaces
+{
+    public interface IExpenseRepository
+    {
+        Task<Expense?> GetByIdAsync(int id);
+        Task<IEnumerable<Expense>> GetAllAsync();
+        Task<IEnumerable<Expense>> GetByUserAsync(int userId);
+        Task<Expense> AddAsync(Expense expense);
+        Task UpdateAsync(Expense expense);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ backend/ExpenseBook.Core/Repositories/ExpenseRepository.cs
@@ -0,0 +1,33 @@
+﻿using ExpenseBook.Core.Interfaces;
+using ExpenseBook.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpenseBook.Core.Repositories
+{
+    public class ExpenseRepository : IExpenseRepository
+    {
+        private readonly AppDbContext _context;
+        public ExpenseRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Expense> AddAsync(Expense expense)
+        {
+            var ent = (await _context.Expenses.Add(expense)).Entity;
+            await _context.SaveChangesAsync();
+            return ent;
+        }
+        public async Task DeleteAsync(int id)
+        {
+            var expense = await _context.Expenses.FindAsync(id);
+            if (expense != null)
+            {
+                _context.Expenses.Remove(expense);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+    }
+}
